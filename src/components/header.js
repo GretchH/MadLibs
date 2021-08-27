ce97905a2425b44ea13c417dc3e73f508aa4563e
GretchH <gretchell.h@gmail.com>
2021-08-27 14:48:46 -0400

    test with terry

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = (title, content) => {
+   return (
+       <div className="header">
+           <h1>Bottega MadLibs</h1>
+           <p>Fill out the fields below and click the generate button to see a madLibs Story.</p>
+       </div>
+   ) 
+}
+
+
+export default Header;
